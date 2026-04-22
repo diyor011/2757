92aa2d4db00a1463b1a4eacf24b1646a291015fe
diyor011 <diyorraxmatullaev3@gmail.com>
2026-04-22 19:27:14 +0500

    islomi kodi

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Aside from './components/Aside'
+import { Outlet } from 'react-router'
+
+const App = () => {
+  return (
+    <div className='flex gap-4 '>
+
+
+
+    
+
+      <Aside/>
+      <div className='p-4'>
+        <Outlet/>
+      </div>
+
+
+    
+    </div>
+  )
+}
+
+export default App
